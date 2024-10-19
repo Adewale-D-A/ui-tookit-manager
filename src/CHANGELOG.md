dc8119a1dc0866efc83b66456e4d2d29f43a28cd
adewale <adewale.d.a@outlook.com>
2024-10-19 18:30:31 +0100

    Input field addition Toggle to show/enable input field

--- src/CHANGELOG.md
+++ src/CHANGELOG.md
@@ -1,3 +1,8 @@
+##### 19/10/2024 (Feature addition - Drag to reorder component)
+
+> Input field addition
+> Toggle to show/enable input field
+
 ##### UI manager setup
 
 > Prompt on page change
--- src/components/drag-to-reorder/index.tsx
+++ src/components/drag-to-reorder/index.tsx
@@ -1,14 +1,22 @@
+import { ChangeEvent, useCallback } from "react";
 import BinIcon from "../../assets/icons/bin";
+import InputFieldIcon from "../../assets/icons/input";
 import "./index.css";
-import { useCallback, useRef } from "react";
 
 export default function DragToReOrder({
   data,
   setData,
   classnames,
   allowDelete,
+  inputConfigration,
 }: {
-  data: { id: string; image?: string; text?: string }[];
+  data: {
+    id: string;
+    image?: string;
+    text?: string;
+    inputFieldText?: string;
+    showInputField?: boolean;
+  }[];
   setData: Function;
   classnames?: {
     parentContainer?: string;
@@ -17,12 +25,19 @@ export default function DragToReOrder({
     text?: string;
     binButton?: string;
     binIcon?: string;
+    input?: string;
+    inputIcon?: string;
+    enableInputIcon?: string;
   };
   allowDelete?: boolean;
+  inputConfigration?: {
+    allowField?: boolean;
+    rows?: number;
+  };
 }) {
   let destinationValue = "";
   let originValue = "";
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  // const containerRef = useRef<HTMLDivElement | null>(null);
 
   // drag destination dataset-index
   const dragEnter = useCallback((e: any) => {
@@ -38,6 +53,7 @@ export default function DragToReOrder({
     return targetIndex || "";
   }, []);
 
+  // handle drag and drop logic
   const dragHandler = useCallback((destination: any, origin: any) => {
     destinationValue = dragEnter(destination) || destinationValue;
     originValue = dragEnd(origin) || originValue;
@@ -75,6 +91,24 @@ export default function DragToReOrder({
     }
   }, []);
 
+  // handle user input
+  const handleUserInput = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>, index: number) => {
+      setData((prev: { inputFieldText: string }[]) => {
+        const originalContent = prev[index];
+        const deecopy = [...prev];
+        deecopy.splice(
+          index, //position to replace
+          1, // number of items to replace
+          { ...originalContent, inputFieldText: e?.target?.value } //replacement value
+        );
+        return deecopy;
+      });
+    },
+    []
+  );
+
+  // remove item from list
   const removeItem = useCallback((index: number) => {
     setData((prev: { id: string }[]) => {
       const deepCopy = [...prev];
@@ -82,11 +116,29 @@ export default function DragToReOrder({
       return deepCopy;
     });
   }, []);
+
+  // toggle input field display
+  const showTextInputField = useCallback((index: number) => {
+    setData((prev: { showInputField: boolean }[]) => {
+      const originalContent = prev[index];
+      const deecopy = [...prev];
+      deecopy.splice(
+        index, //position to replace
+        1, // number of items to replace
+        {
+          ...originalContent,
+          showInputField: Boolean(!originalContent?.showInputField),
+        } //replacement value
+      );
+      return deecopy;
+    });
+  }, []);
+
   return (
     <div
       className={classnames?.parentContainer || "aad-dr-d-grid"}
       draggable
-      ref={containerRef}
+      // ref={containerRef}
     >
       {data?.map((item, index) => (
         <div
@@ -100,6 +152,7 @@ export default function DragToReOrder({
           onDragEnter={(e) => dragHandler(e, undefined)} //get destination index
           onDragEnd={(e) => dragHandler(undefined, e)} //get drag origin
         >
+          {/* only for image */}
           {item?.image && (
             <img
               src={item?.image}
@@ -107,18 +160,58 @@ export default function DragToReOrder({
               className={classnames?.image || " aad-dr-w-full"}
             />
           )}
+          {/* only for text */}
           {item?.text && (
             <span className={classnames?.text || "aad-dr-tex"}>
               {item?.text}
             </span>
           )}
+          {/* only for feature to delete  */}
           {allowDelete && (
             <button
               onClick={() => removeItem(index)}
               className={classnames?.binButton || "aad-dr-bin-icon-button"}
+              title="delete item"
             >
               <BinIcon className={classnames?.binIcon || "aad-dr-bin-icon"} />
             </button>
+          )}
+          {/* only when input field is anabled by each item */}
+          {inputConfigration?.allowField && (
+            <button
+              onClick={() => showTextInputField(index)}
+              className={
+                classnames?.binButton || "aad-dr-text-field-icon-button"
+              }
+              title="toggle input field display"
+            >
+              {/* use default class,  if no class is provided. if text field is enabled for 
+              this item and custom class is passed, use custom class, else us ddefault class 
+              and if field is not enabled, remove icon styling              
+              */}
+
+              <InputFieldIcon
+                className={`${classnames?.inputIcon || "aad-dr-bin-icon"} ${
+                  item?.showInputField && classnames?.enableInputIcon
+                    ? classnames?.enableInputIcon
+                    : item?.showInputField
+                    ? "add-dr-input-icon-enabled"
+                    : ""
+                }`}
+              />
+            </button>
+          )}
+          {/* show input field when option is enabled and item contains field for input text */}
+          {inputConfigration?.allowField && item?.showInputField && (
+            <textarea
+              id={item?.id}
+              value={item?.inputFieldText || ""}
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                handleUserInput(e, index)
+              }
+              rows={inputConfigration?.rows || 1}
+              className={classnames?.input || "aad-dr-input-field"}
+            />
           )}
         </div>
       ))}
